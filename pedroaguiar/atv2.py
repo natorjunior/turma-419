000a29e3d550e8d5a986f4e8c2c7754a057ebc2e
Pedro Aguiar <pedroaguiar1401@gmail.com>
2023-06-14 21:36:42 -0300

    Atividade 2 e 3 - Resolução

---
+++ pedroaguiar/atv2.py
@@ -0,0 +1,9 @@
+lista = []
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+nota3 = int(input("Digite a teceira nota: "))
+nota4 = int(input("Digite a quarta nota: "))
+lista = [nota1, nota2, nota3, nota4]
+media = int(nota1+nota2+nota3+nota4)/4
+print(f"As notas são: 1:  {nota1}, 2:  {nota2}, 3:  {nota3}, 4: {nota4} e a media delas é: {media:.2f}")
+
---
+++ pedroaguiar/atv3.py
@@ -0,0 +1,5 @@
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [6, 7, 8, 9, 10]
+
+soma = lista1[0] + lista2[-1]
+print(soma)
