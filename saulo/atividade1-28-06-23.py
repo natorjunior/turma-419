fa0187ea538fa2dcf9b45286491476283c125819
Saolim <88401165+Saolim@users.noreply.github.com>
2023-06-28 19:57:06 -0300

    Update atividade1-28-06-23.py

--- saulo/atividade1-28-06-23.py
+++ saulo/atividade1-28-06-23.py
@@ -1,5 +1,4 @@
-lista = []
-# for i in range(0,4):
+lista = [] 
 while True:
     validar = input("Deseja continuar inserindo notas? Digite S para continuar e N para encerrar: ")
     if validar == "S":
@@ -7,7 +6,7 @@ while True:
         media = sum(lista) / len(lista)
 
     else:
-        a= str(lista).replace('[','').replace(']','')
+        a= str(lista).replace('[','').replace(']','') #Remove os colchetes da lista para exibi-la de forma amigável.
         print(f"As notas bimestrais do aluno foram respectivamente {a}. "
               f"A média do aluno foi {media} ")
         break
