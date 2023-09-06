5c3da9123ba2dc8d076b086e91975ab6749505e3
Dominy Martins <122042813+DominyMartins@users.noreply.github.com>
2023-09-06 20:06:50 -0300

    Update Aula 12 - Extra SQL.sql
    incluído o comentários das questões.

--- Dominy/Aula 12/Aula 12 - Extra SQL.sql
+++ Dominy/Aula 12/Aula 12 - Extra SQL.sql
@@ -1,10 +1,12 @@
+/* 1- Escreva uma consulta SQL para calcular a média salarial por departamento. Apresente o resultado com o nome do departamento e a média salarial.*/
 SELECT Departamento, AVG(Salario) AS MediaSalarial
 FROM Funcionarios
 GROUP BY Departamento;
-
+/* 2- Escreva uma consulta para mostrar todos os cargos existentes na tabela, sem que ocorra duplicidade.  */
 SELECT Cargo FROM Funcionarios
 GROUP BY Cargo;
 
+/* 3- Escreva uma consulta SQL para encontrar os funcionários com os salários mais altos em cada departamento. Apresente o nome, cargo e salário de cada funcionário.*/
 SELECT F.Nome, F.Cargo, F.Salario
 FROM Funcionarios F
 INNER JOIN (
